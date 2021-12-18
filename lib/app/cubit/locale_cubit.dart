0369c2672ff2572bc2dc04d7b49619c3f5cb845f
Lucas Alexandre Fell <47724710+fell-lucas@users.noreply.github.com>
2021-12-18 18:38:04 -0300

    updates locale cubit

--- lib/app/cubit/locale_cubit.dart
+++ lib/app/cubit/locale_cubit.dart
@@ -6,8 +6,8 @@ import 'package:portfolio/l10n/l10n.dart';
 class LocaleCubit extends Cubit<Locale> {
   LocaleCubit()
       : super(
-          AppLocalizations.supportedLocales.contains(Locale(Intl.getCurrentLocale()))
-              ? Locale(Intl.getCurrentLocale())
+          AppLocalizations.supportedLocales.contains(Locale(Intl.getCurrentLocale().split('_')[0]))
+              ? Locale(Intl.getCurrentLocale().split('_')[0])
               : const Locale('en'),
         );
 
