0884415586a53574c2e58852babbe1286d0f0a79
Lucas Alexandre Fell <47724710+fell-lucas@users.noreply.github.com>
2021-12-18 23:50:08 -0300

    adds dark theme support

--- lib/app/constants/dark_theme.dart
+++ lib/app/constants/dark_theme.dart
@@ -1,4 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:portfolio/app/app.dart';
+import 'package:portfolio/app/constants/shared_theme.dart';
 
-final darkThemeData = lightThemeData.copyWith(brightness: Brightness.dark);
+final darkThemeData = lightThemeData.copyWith(
+  scaffoldBackgroundColor: Colors.blueGrey.shade700,
+  colorScheme: darkColorScheme,
+  appBarTheme: appBarTheme,
+  textTheme: textTheme.apply(bodyColor: Colors.white),
+  iconTheme: iconTheme,
+  outlinedButtonTheme: outlinedButtonTheme,
+  canvasColor: Colors.blueGrey,
+  brightness: Brightness.dark,
+);
+
+final darkColorScheme = ColorScheme.fromSwatch(
+  backgroundColor: Colors.blueGrey.shade700,
+  errorColor: Colors.red,
+  primarySwatch: MaterialColor(Colors.blueGrey.shade100.value, {}),
+  accentColor: accentColor,
+  primaryColorDark: Colors.blueGrey.shade700,
+  cardColor: accentColor,
+  brightness: Brightness.dark,
+);
---
+++ lib/app/constants/shared_theme.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+final textTheme = TextTheme(
+  headline1: GoogleFonts.firaCode(fontSize: 98, fontWeight: FontWeight.w300, letterSpacing: -1.5),
+  headline2: GoogleFonts.firaCode(fontSize: 61, fontWeight: FontWeight.w300, letterSpacing: -0.5),
+  headline3: GoogleFonts.firaCode(fontSize: 49, fontWeight: FontWeight.w400),
+  headline4: GoogleFonts.firaCode(fontSize: 35, fontWeight: FontWeight.w400, letterSpacing: 0.25),
+  headline5: GoogleFonts.firaCode(fontSize: 24, fontWeight: FontWeight.w400),
+  headline6: GoogleFonts.firaCode(fontSize: 20, fontWeight: FontWeight.w500, letterSpacing: 0.15),
+  subtitle1: GoogleFonts.firaCode(fontSize: 16, fontWeight: FontWeight.w400, letterSpacing: 0.15),
+  subtitle2: GoogleFonts.firaCode(fontSize: 14, fontWeight: FontWeight.w500, letterSpacing: 0.1),
+  bodyText1: GoogleFonts.firaCode(fontSize: 16, fontWeight: FontWeight.w400, letterSpacing: 0.5),
+  bodyText2: GoogleFonts.firaCode(fontSize: 14, fontWeight: FontWeight.w400, letterSpacing: 0.25),
+  button: GoogleFonts.firaCode(fontSize: 14, fontWeight: FontWeight.w500, letterSpacing: 1.25),
+  caption: GoogleFonts.firaCode(fontSize: 12, fontWeight: FontWeight.w400, letterSpacing: 0.4),
+  overline: GoogleFonts.firaCode(fontSize: 10, fontWeight: FontWeight.w400, letterSpacing: 1.5),
+);
