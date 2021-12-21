36f26161c2ca8737685a55acfc3af9a1f03a98e2
Lucas Alexandre Fell <47724710+fell-lucas@users.noreply.github.com>
2021-12-20 22:49:37 -0300

    improves dark theme support

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,10 +1,2 @@
-// Copyright (c) 2021, Very Good Ventures
-// https://verygood.ventures
-//
-// Use of this source code is governed by an MIT-style
-// license that can be found in the LICENSE file or at
-// https://opensource.org/licenses/MIT.
-
-export 'constants/dark_theme.dart';
-export 'constants/light_theme.dart';
+export 'constants/constants.dart';
 export 'view/app.dart';
--- lib/app/constants/light_theme.dart
+++ lib/app/constants/light_theme.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:portfolio/app/constants/shared_theme.dart';
+import 'package:portfolio/app/constants/constants.dart';
 
 final lightThemeData = ThemeData(
   scaffoldBackgroundColor: Colors.blueGrey.shade100,
@@ -12,11 +12,6 @@ final lightThemeData = ThemeData(
   brightness: Brightness.light,
 );
 
-const appBarTheme = AppBarTheme(
-  backgroundColor: Colors.transparent,
-  elevation: 0,
-);
-
 final colorScheme = ColorScheme.fromSwatch(
   backgroundColor: Colors.blueGrey.shade100,
   errorColor: Colors.red,
@@ -24,19 +19,4 @@ final colorScheme = ColorScheme.fromSwatch(
   accentColor: accentColor,
   primaryColorDark: Colors.blueGrey.shade700,
   cardColor: accentColor,
-);
-
-const accentColor = Colors.deepOrange;
-
-const iconTheme = IconThemeData(
-  size: 18,
-  color: accentColor,
-);
-
-final outlinedButtonTheme = OutlinedButtonThemeData(
-  style: OutlinedButton.styleFrom(
-    side: const BorderSide(
-      color: accentColor,
-    ),
-  ),
 );
