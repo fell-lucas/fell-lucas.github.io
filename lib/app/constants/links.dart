8bca4185c71109cc5895cc774a730380b30c147a
Lucas Alexandre Fell <47724710+fell-lucas@users.noreply.github.com>
2021-12-20 23:21:12 -0300

    email translations

--- lib/app/constants/links.dart
+++ lib/app/constants/links.dart
@@ -2,5 +2,4 @@ const kGithub = 'https://github.com/fell-lucas';
 const kTwitter = 'https://twitter.com/pryftas';
 const kLinkedIn = 'https://www.linkedin.com/in/lucas-fell/';
 const kPlayStore = 'https://play.google.com/store/apps/developer?id=Lucas+Fell';
-const kEmail =
-    "mailto:lucasafell@gmail.com?subject=Hey! What's up?&body=I just came from your website and I'd like your opinion on...";
+const kEmail = 'mailto:lucasafell@gmail.com';
