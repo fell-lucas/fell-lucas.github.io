6869d21d3eab9133162d017ae4e89223bf39a1b8
fell-lucas <47724710+fell-lucas@users.noreply.github.com>
2021-12-26 18:22:40 +0000

    Deploying to gh-pages from  @ 8b43c64e0cb604020db3ee560b5ce39be9a50f50 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -20,7 +20,7 @@ const RESOURCES = {
 "splash/img/dark-4x.png": "6d3345aed9c34604507e4999ef3b98db",
 "splash/img/light-2x.png": "a6d5ebf47fe747e1045c07b483936c74",
 "splash/style.css": "5c74776d35b9e85d790997a9f2349a2d",
-"manifest.json": "a453a6f5df223462211329f823e068cd",
+"manifest.json": "1b0e6d1315cc329beba58f2bfa669287",
 "icons/favicon.ico": "608b5016b351a0905cd09d2aa9561bc1",
 "icons/icon-512-maskable.png": "17679abb2bbcad2b6a5a0ef9b1bc1e53",
 "icons/Icon-512.png": "5f1531c5df732d8df2a2186281f3b5b8",
@@ -34,8 +34,8 @@ const RESOURCES = {
 "assets/packages/font_awesome_flutter/lib/fonts/fa-brands-400.ttf": "b37ae0f14cbc958316fac4635383b6e8",
 "assets/packages/font_awesome_flutter/lib/fonts/fa-regular-400.ttf": "5178af1d278432bec8fc830d50996d6f",
 "assets/packages/font_awesome_flutter/lib/fonts/fa-solid-900.ttf": "aa1ec80f1b30a51d64c72f669c1326a7",
-"index.html": "e7c1f6d2e16b2af3c83c07921af92ceb",
-"/": "e7c1f6d2e16b2af3c83c07921af92ceb"
+"index.html": "2828259f8fb606982e49ccf894f0614c",
+"/": "2828259f8fb606982e49ccf894f0614c"
 };
 
 // The application shell files that are downloaded before a service worker can
