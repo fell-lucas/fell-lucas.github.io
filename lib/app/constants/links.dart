e2155767e67c1e82e93472c24caada75d4ab283c
Lucas Alexandre Fell <lucasafell@gmail.com>
2021-12-26 20:04:09 -0300

    updates twitter handle

--- lib/app/constants/links.dart
+++ lib/app/constants/links.dart
@@ -1,5 +1,5 @@
 const kGithub = 'https://github.com/fell-lucas';
-const kTwitter = 'https://twitter.com/pryftas';
+const kTwitter = 'https://twitter.com/_electrictears';
 const kLinkedIn = 'https://www.linkedin.com/in/lucas-fell/';
 const kPlayStore = 'https://play.google.com/store/apps/developer?id=Lucas+Fell';
 const kEmail = 'lucasafell@gmail.com';
